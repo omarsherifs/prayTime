c3813c996c68bcc1ee18fa109d1a6a60a5a9536d
omar sherif <omar.sherifs@live.com>
2019-12-29 16:36:47 +0200

    final

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,4 +1,3 @@
-
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -14,49 +13,31 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List x = ["الفجر", "الشروق", "الظهر", "العصر", "االمغرب", "العشاء"];
-  List y;
-  PrayTime times;
+  List y = new List();
+  PrayTime times = new PrayTime();
 
-  initState(){
-
-
+  void initState() {
+    super.initState();
+    getTimes();
   }
 
   getTimes() async {
     DateTime now = DateTime.now();
     String formattedDate = DateFormat('yyyy-MM-dd').format(now);
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    await times.getPrayTimes((position.longitude).toString(), (position.latitude).toString(),formattedDate);
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    await times.getPrayTimes((position.longitude).toString(),
+        (position.latitude).toString(), formattedDate);
     y.add(times.fajr);
+    y.add(times.sunrise);
     y.add(times.dhuhr);
     y.add(times.asr);
     y.add(times.maghrib);
     y.add(times.isha);
-
+    setState(() {});
   }
 
-  
-
   Widget build(BuildContext context) {
-    Widget item(int i) {
-      return Container(
-        padding: EdgeInsets.all(12),
-        width: MediaQuery.of(context).size.width,
-        height: 50,
-        color: Colors.blue.withOpacity(0.2),
-        child: Row(
-          children: <Widget>[
-            SizedBox(height: 10),
-            Text(y[i], style: TextStyle(fontSize: 18, color: Colors.white)),
-            Expanded(
-              child: Container(),
-            ),
-            Text(x[i], style: TextStyle(fontSize: 18, color: Colors.white)),
-          ],
-        ),
-      );
-    }
-
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -76,68 +57,36 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          Column(
-            children: <Widget>[
-              Container(
-                //height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                //color: Colors.amber,
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height / 10),
-                child: Text(
-                  'تواقيت الصلاة',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 25, color: Colors.white),
-                ),
-              ),
-              Divider(
-                  color: Colors.white,
-                  thickness: 1.5,
-                  endIndent: 70,
-                  indent: 70),
-              SizedBox(height: MediaQuery.of(context).size.height / 5),
-              item(0),
-              SizedBox(height: 10),
-              item(1),
-              SizedBox(height: 10),
-              item(2),
-              SizedBox(height: 10),
-              item(3),
-              SizedBox(height: 10),
-              item(4),
-              SizedBox(height: 10),
-              item(5),
-              SizedBox(height: 20),
-              Container(
-                height: 70,
-                color: Colors.grey.withOpacity(0.3),
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.only(
-                  top: 12,
-                  left: 12,
-                  right: 12,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      '10:24:04',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                      textAlign: TextAlign.center,
+          ListView.builder(
+            itemCount: y.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: <Widget>[
+                  Container(
+                    height: MediaQuery.of(context).size.height / 8,
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.blue.withOpacity(0.2),
+                    child: Row(
+                      children: <Widget>[
+                        SizedBox(height: 10),
+                        Text(y[index],
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.white)),
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Text(x[index],
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.white)),
+                      ],
                     ),
-                    Expanded(
-                      child: Container(),
-                    ),
-                    Text(
-                      'السبت 15/12/2019',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                  SizedBox(height: 10),
+                ],
+              );
+            },
+          )
         ],
       ),
     );
--- lib/prayTimeController.dart
+++ lib/prayTimeController.dart
@@ -2,34 +2,37 @@ import 'package:dio/dio.dart';
 
 class PrayTime {
   String fajr;
+  String sunrise;
   String dhuhr;
   String asr;
   String maghrib;
   String isha;
-  Dio dio;
+  Dio dio = new Dio();
   Response res;
 
-  PrayTime({this.fajr, this.dhuhr, this.asr, this.maghrib, this.isha});
+  PrayTime({this.fajr, this.sunrise, this.dhuhr, this.asr, this.maghrib, this.isha});
 
   getPrayTimes(
       String longitude, String latitude, String date) async {
-   try { res = await dio.get(
+   //try { 
+     res = await dio.get(
         "https://api.pray.zone/v2/times/day.json?longitude=$longitude&latitude=$latitude&date=$date");
 
       this.fajr = res.data["results"]["datetime"][0]["times"]["Fajr"];
+      this.sunrise =  res.data["results"]["datetime"][0]["times"]["Sunrise"];
       this.dhuhr = res.data["results"]["datetime"][0]["times"]["Dhuhr"];
       this.asr = res.data["results"]["datetime"][0]["times"]["Asr"];
       this.maghrib = res.data["results"]["datetime"][0]["times"]["Maghrib"];
       this.isha = res.data["results"]["datetime"][0]["times"]["Isha"];
   
-    }
-    catch(e){
-      this.fajr = "--:--";
-      this.dhuhr =  "--:--";
-      this.asr =  "--:--";
-      this.maghrib =  "--:--";
-      this.isha =  "--:--";
-    }
+    // }
+    // catch(e){
+    //   this.fajr = "--:--";
+    //   this.dhuhr =  "--:--";
+    //   this.asr =  "--:--";
+    //   this.maghrib =  "--:--";
+    //   this.isha =  "--:--";
+    // }
 
 
   }
